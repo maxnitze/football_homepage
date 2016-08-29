8a3fcb4763acf572f8ff463566286fa3a9835270
Max Nitze <maxnitze@cs.uni-bremen.de>
2016-08-29 19:34:40 +0200

    Fix for migration files

--- db/migrate/20140903133208_create_news.rb
+++ db/migrate/20140903133208_create_news.rb
@@ -1,6 +1,10 @@
 class CreateNews < ActiveRecord::Migration
   def change
     create_table :news do |t|
+      t.string :title,      null: false
+      t.string :subtitle,   default: '', null: false
+      t.string :abstract,   default: '', null: false
+      t.string :text,       default: '', null: false
       t.integer :author_id
       t.integer :editor_id
       t.integer :edit_count, default: 0, null: false
