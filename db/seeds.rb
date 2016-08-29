888f23a75934f98da8d3f31a4e03f264a7f06270
Max Nitze <maxnitze@cs.uni-bremen.de>
2016-08-29 20:35:55 +0200

    Heroku preparations

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 %w{
   user_role_permissions user_roles
 }.each do |part|
-  load File.expand_path(__dir__)+"/seeds/#{part}.rb"
+  load "#{File.expand_path(__dir__)}/seeds/#{part}.rb"
 end
 
 # testdata, no real seeds
 %w{
   users teams clubs players leagues news
 }.each do |part|
-  load File.expand_path(__dir__)+"/test_data/#{part}.rb"
+  load "#{File.expand_path(__dir__)}/test_data/#{part}.rb"
 end
