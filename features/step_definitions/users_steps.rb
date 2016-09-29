dab852a5d5c9ef8d86140704f49196ae63b011f5
Max Nitze <maxnitze@informatik.uni-bremen.de>
2016-09-29 22:04:12 +0200

    Locale workaround for user login
    New created users are have always locale :en, to overcome the default
    url options problem in all cucumber features.

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -1,7 +1,8 @@
 # feature/step_definitions/users_steps.rb
 
+# TODO remove locale when localization is fixed
 Given /^there is a user$/ do
-  @user = FactoryGirl.create :user
+  @user = FactoryGirl.create :user, locale: :en
   @user.confirmed_at = Time.zone.now
   @user.save
 end
@@ -11,8 +12,9 @@ Given /^there is a logged in user$/ do
   step 'the user is logged in'
 end
 
+# TODO remove locale when localization is fixed
 Given /^there is a user named (.+)$/ do |name|
-  @user = FactoryGirl.create :user, name: name
+  @user = FactoryGirl.create :user, name: name, locale: :en
   @user.confirmed_at = Time.zone.now
   @user.save
 end
