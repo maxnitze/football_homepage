43e568213e5f31318ceca8bed1703ca304730d47
Max Nitze <maxnitze@informatik.uni-bremen.de>
2016-10-06 21:03:11 +0200

    Echo to echo -e

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -5,13 +5,13 @@ set -ev
 
 # create mysql database if on mysql job
 if [ "${DB}" = "mysql" ]; then
-  echo "Creating mysql database"
+  echo -e "Creating mysql database"
   mysql -e 'create database my_app_test'
 # create postgresql database if on mysql job
 elif [ "${DB}" = "postgresql" ]; then
-  echo "Creating postgresql database"
+  echo -e "Creating postgresql database"
   psql -c 'create database my_app_test' -U postgres
 else
-  echo "No database initialized"
+  echo -e "No database initialized"
   exit 0
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -5,7 +5,7 @@ set -ev
 
 # run bundle install when not on COVERITY_SCAN_BRANCH
 if [ "${TRAVIS_BRANCH}" != "${COVERITY_SCAN_BRANCH}" ]; then
-  echo "On branch '${TRAVIS_BRANCH}'\nExecuting bundle install"
+  echo -e "On branch '${TRAVIS_BRANCH}'\nExecuting bundle install"
   bundle install --jobs=3 --retry=3 --without production development
 else
   exit 0
