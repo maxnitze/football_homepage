1e538ae126c3032f44f092ac786ebf2f266f96b5
Max Nitze <maxnitze@informatik.uni-bremen.de>
2016-10-06 19:06:01 +0200

    Make .travis scripts executable before running

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 # stop script if single command fails
 set -ev
 
+# run bundle install when not on COVERITY_SCAN_BRANCH
 if [ "${TRAVIS_BRANCH}" != "${COVERITY_SCAN_BRANCH}" ]; then
-  # run bundle install when not on COVERITY_SCAN_BRANCH
   bundle install --jobs=3 --retry=3 --without production development
 fi
