a93f1d3345118c8d1fc50e6438109e19e99dd430
Max Nitze <max.nitze@gmx.de>
2017-04-20 15:01:43 +0200

    Squadleagueteams are supplied
    Test, that a league_team supplies a list of possible squadleagueteams.

--- spec/controllers/general_static_pages_controller_spec.rb
+++ spec/controllers/general_static_pages_controller_spec.rb
@@ -1,30 +1,31 @@
+# spec/controllers/general_static_pages_controller_spec.rb
 require 'rails_helper'
 
-RSpec.describe GeneralStaticPagesController, :type => :controller do
+RSpec.describe GeneralStaticPagesController, type: :controller do
 
-  describe "GET contact" do
-    it "returns http success" do
+  describe 'GET contact' do
+    it 'returns http success' do
       get :contact
       expect(response).to be_success
     end
   end
 
-  describe "GET help" do
-    it "returns http success" do
+  describe 'GET help' do
+    it 'returns http success' do
       get :help
       expect(response).to be_success
     end
   end
 
-  describe "GET home" do
-    it "returns http success" do
+  describe 'GET home' do
+    it 'returns http success' do
       get :home
       expect(response).to be_success
     end
   end
 
-  describe "GET site_notice" do
-    it "returns http success" do
+  describe 'GET site_notice' do
+    it 'returns http success' do
       get :site_notice
       expect(response).to be_success
     end
--- spec/models/league_team_spec.rb
+++ spec/models/league_team_spec.rb
@@ -96,4 +96,31 @@ RSpec.describe LeagueTeam, type: :model do
   it 'is not valid without an unsubscribed value' do
     expect(build :league_team, unsubscribed: nil).to_not be_valid
   end
+
+  it 'supplies a list of possible squadleagueteams' do
+    team = create :team
+    league_team = create :league_team, team: team
+
+    # first possible league team
+    ltp1_start = league_team.league.start + (Faker::Boolean.boolean ? 1 : -1) * Faker::Number.between(0, 11).months
+    league_possible_1 = create :league, start: ltp1_start, end: (ltp1_start + 1.year)
+    league_team_possible_1 = create :league_team, team: team, league: league_possible_1
+    # second possible league team
+    ltp2_start = league_team.league.start + (Faker::Boolean.boolean ? 1 : -1) * Faker::Number.between(0, 11).months
+    league_possible_2 = create :league, start: ltp2_start, end: (ltp2_start + 1.year)
+    league_team_possible_2 = create :league_team, team: team, league: league_possible_2
+
+    # first not possible league team
+    lti1_start = league_team.league.start + (Faker::Boolean.boolean ? 1 : -1) * Faker::Number.between(1, 20).years
+    league_impossible_1 = create :league, start: lti1_start, end: (lti1_start + 1.year)
+    league_team_impossible_1 = create :league_team, team: team, league: league_impossible_1
+    # second not possible league team
+    lti2_start = league_team.league.start + (Faker::Boolean.boolean ? 1 : -1) * Faker::Number.between(1, 20).years
+    league_impossible_2 = create :league, start: lti2_start, end: (lti2_start + 1.year)
+    league_team_impossible_2 = create :league_team, team: team, league: league_impossible_2
+
+    possible_squadleagueteams = league_team.get_possible_squadleagueteams
+    expect(possible_squadleagueteams).to include(league_team_possible_1, league_team_possible_2)
+    expect(possible_squadleagueteams).to_not include(league_team_impossible_1, league_team_impossible_2)
+  end
 end
